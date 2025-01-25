dd34d72899736336fc53c94a605770cf710a42d7
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2025-01-25 14:53:09 -0700

    coolpics

---
+++ coolpics/coolpics.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-button').addEventListener('click', () => {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.hidden = !navLinks.hidden;
+  });
+  
